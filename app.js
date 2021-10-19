1b57b1b02ad6e56e68d46022173e93ec1df9d62a
Wajahat-Ahmed-NED <wajahatahmedwajahat@gmail.com>
2021-10-19 21:24:42 +0500

    3rd Commit

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 console.log("Welcome to Tic Tac Toe Game")
-let music=new Audio("music.mp3")
+let audioTurn=new Audio("ting.mp3")
 let gameover=new Audio("gameover.mp3")
 let turn = "X"
 
 const changeTurn=()=>{
-    return turn==="X"?"0":"X"
+    return turn==="X"?"O":"X"
 }
 
 const checkWin=()=>{
@@ -13,12 +13,14 @@ const checkWin=()=>{
 let boxes=document.getElementsByClassName("box")
 
 Array.from(boxes).forEach(element=>{
-    let boxtext=document.querySelector('.boxtext')
-    element.addeventListener('click',()=>{
+    let boxtext=element.querySelector('.boxtext')
+    element.addEventListener('click',()=>{
         if(boxtext.innerText===""){
         boxtext.innerText=turn
         turn=changeTurn()
-        music.play()
+        console.log("Before")
+        audioTurn.play()
+        console.log("after")
         checkWin();
         document.getElementsByClassName('info')[0].innerText="Turn For "+turn;}
     })
